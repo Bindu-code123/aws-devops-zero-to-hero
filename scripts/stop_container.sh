7547b5099b3337eefd13544a298c8164ed686aac
Bindu-code123 <binduvemavarapu2009@gmail.com>
2026-07-30 00:19:04 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+echo "Stopping existing containers..."
+
+CONTAINER_IDS=$(docker ps -aq)
+
+if [ -n "$CONTAINER_IDS" ]; then
+    docker rm -f $CONTAINER_IDS
+    echo "Containers removed successfully."
+else
+    echo "No containers to stop."
+fi
